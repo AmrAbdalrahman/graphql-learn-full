c692f97b5d807ff31670dab35c5da11f8c156e60
AmrAlmagic <amr.abdalrahman94@gmail.com>
2019-03-09 16:16:15 +0200

    create custom type

--- graphql-basics/src/index.js
+++ graphql-basics/src/index.js
@@ -3,23 +3,61 @@ import {GraphQLServer} from "graphql-yoga";
 
 //Type definition (schema)
 const typeDefs = `
-    type Query {
-        hello: String!
+     type Query {
+        users(query: String): [User!]!
+        posts(query: String): [Post!]!
+        comments: [Comment!]!
+        me: User!
+        post: Post!
+    }
+    
+     type User {
+        id: ID!
         name: String!
+        email: String!
+        age: Int
+        posts: [Post!]!
+        comments: [Comment!]!
+    }
+
+    type Post {
+        id: ID!
+        title: String!
+        body: String!
+        published: Boolean!
+        author: User!
+        comments: [Comment!]!
+    }
+
+    type Comment {
+        id: ID!
+        text: String!
+        author: User!
+        post: Post!
     }
 `
 
 // Resolves
 const resolvers = {
     Query: {
-        hello() {
-            return 'this is my first query';
+        me() {
+           return {
+               id: '123',
+               name: 'amr',
+               email: 'ass@fr.com',
+               age: 25
+           }
         },
-        name() {
-            return 'my name is amr almagic';
+        post() {
+            return {
+                id: '092',
+                title: 'GraphQL 101',
+                body: '',
+                published: false
+            }
         }
     }
-}
+};
 
 const server = new GraphQLServer({
    typeDefs,
