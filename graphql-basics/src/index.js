d19950123882004796ee622fe7d95a82344727fd
AmrAlmagic <amr.abdalrahman94@gmail.com>
2019-03-11 21:59:41 +0200

    works with create input

--- graphql-basics/src/index.js
+++ graphql-basics/src/index.js
@@ -1,4 +1,5 @@
 import {GraphQLServer} from "graphql-yoga";
+import uuidv4 from 'uuid/v4';
 
 // Demo user data
 const users = [{
@@ -68,6 +69,32 @@ const typeDefs = `
         post: Post!
     }
     
+    
+    type Mutation {
+        createUser(data: CreateUserInput): User!
+        createPost(data: CreatePostInput!): Post!
+        createComment(data: CreateCommentInput!): Comment!
+    }
+    
+    input CreateUserInput {
+        name: String!
+        email: String!
+        age: Int
+    }
+    
+    input CreatePostInput {
+    title: String!
+    body: String!
+    published: Boolean!
+    author: ID!
+}
+
+input CreateCommentInput {
+    text: String!
+    author: ID!
+    post: ID!
+}
+    
      type User {
         id: ID!
         name: String!
@@ -135,14 +162,109 @@ const resolvers = {
                 published: false
             }
         }
+    },
+    Mutation: {
+        createUser(parent, args, ctx, info) {
+            const emailTaken = users.some((user) =>{
+                return user.email === args.email;
+            });
+
+            if(emailTaken){
+                throw new Error('Email taken.');
+            }
+
+            const one = {
+
+            }
+
+            const user = {
+                id: uuidv4(),
+                name: args.name,
+                email: args.email,
+                age: args.age
+            };
+            users.push(user);
+            return user;
+        },
+        createPost(parent, args, ctx, info){
+            const userExists = users.some((user) => user.id === args.author)
+
+            if (userExists){
+                throw  new Error('User not found');
+            }
+
+            const post = {
+                id: uuidv4(),
+                title: args.title,
+                body: args.body,
+                published: args.published,
+                author: args.author,
+            }
+            posts.push(post)
+            return post;
+        },
+        createComment(parent, args, { db }, info) {
+            const userExists = db.users.some((user) => user.id === args.data.author)
+            const postExists = db.posts.some((post) => post.id === args.data.post && post.published)
+
+            if (!userExists || !postExists) {
+                throw new Error('Unable to find user and post')
+            }
+
+            const comment = {
+                id: uuidv4(),
+                ...args.data
+            }
+
+            db.comments.push(comment)
+
+            return comment
+        }
+    },
+
+    Post: {
+        author(parent, args, ctx, info) {
+            return users.find((user) => {
+                return user.id === parent.author
+            })
+        },
+        comments(parent, args, ctx, info) {
+            return comments.filter((comment) => {
+                return comment.post === parent.id
+            })
+        }
+    },
+    Comment: {
+        author(parent, args, ctx, info) {
+            return users.find((user) => {
+                return user.id === parent.author
+            })
+        },
+        post(parent, args, ctx, info) {
+            return posts.find((post) => {
+                return post.id === parent.post
+            })
+        }
+    },
+    User: {
+        posts(parent, args, ctx, info) {
+            return posts.filter((post) => {
+                return post.author === parent.id
+            })
+        },
+        comments(parent, args, ctx, info) {
+            return comments.filter((comment) => {
+                return comment.author === parent.id
+            })
+        }
     }
 };
 
 const server = new GraphQLServer({
-   typeDefs,
-   resolvers
+    typeDefs,
+    resolvers
 });
 
-server.start(()=>{
+server.start(() => {
     console.log('the server is up');
 });
