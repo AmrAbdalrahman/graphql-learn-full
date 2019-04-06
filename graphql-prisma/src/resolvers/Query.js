046f0c63382cb989503ab1c3acac65089993daf7
AmrAlmagic <amr.abdalrahman94@gmail.com>
2019-04-06 14:14:23 +0200

    sort and pagination risma

--- graphql-prisma/src/resolvers/Query.js
+++ graphql-prisma/src/resolvers/Query.js
@@ -1,7 +1,13 @@
 import getUserId from '../utils/getUserId'
+
 const Query = {
     users(parent, args, { prisma }, info) {
-        const opArgs = {}
+        const opArgs = {
+            first: args.first,
+            skip: args.skip,
+            after: args.after,
+            orderBy: args.orderBy
+        }
 
         if (args.query) {
             opArgs.where = {
@@ -16,6 +22,10 @@ const Query = {
     myPosts(parent, args, { prisma, request }, info) {
         const userId = getUserId(request)
         const opArgs = {
+            first: args.first,
+            skip: args.skip,
+            after: args.after,
+            orderBy: args.orderBy,
             where: {
                 author: {
                     id: userId
@@ -35,6 +45,10 @@ const Query = {
     },
     posts(parent, args, { prisma }, info) {
         const opArgs = {
+            first: args.first,
+            skip: args.skip,
+            after: args.after,
+            orderBy: args.orderBy,
             where: {
                 published: true
             }
